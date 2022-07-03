6a6963222a8575751d6872ee998b9504572fa493
Ivan Sabat <sabotazh001@gmail.com>
2022-07-03 11:04:05 +0300

    Added Chapter 26 – Orders and Items

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,7 +2,6 @@
 
 namespace App\Models;
 
-use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -24,6 +23,7 @@ class User extends Authenticatable
      * $this->attributes['balance']             - int       - contains the user balance
      * $this->attributes['created_at']          - timestamp - contains the user creation date
      * $this->attributes['updated_at']          - timestamp - contains the user update date
+     * $this->orders                            - Order[]   - contains the associated orders
      */
 
     /**
@@ -135,5 +135,20 @@ class User extends Authenticatable
     public function setUpdatedAt($updatedAt)
     {
         $this->attributes['updated_at'] = $updatedAt;
+    }
+
+    public function orders()
+    {
+        return $this->hasMany(Order::class);
+    }
+
+    public function getOrders()
+    {
+        return $this->orders;
+    }
+
+    public function setOrders($orders)
+    {
+        $this->orders = $orders;
     }
 }
